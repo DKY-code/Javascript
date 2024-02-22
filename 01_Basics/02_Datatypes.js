be35346ba7acbd89ef7f46c876a4ba30ba14529c
Deepak Yadav <iamdky2017@gmail.com>
2024-02-22 09:58:44 +0530

    Remarkds update

--- 01_Basics/02_Datatypes.js
+++ 01_Basics/02_Datatypes.js
@@ -1,16 +1,18 @@
 console.log("Deepak")
 let name = "vikas"
 let age = 19
-let isLoggedIn = false
+let isLoggedIn = true
 
 // alert(2+2) // We are using nodejs, not browser
 /* number => 2 to power 53
 bigint
 string => "" Should write in double quotes
-boolean => true/false
-null => standalone  value
+boolean => true/false.
+null => standalone  value.
 undefined => 
-symbol => It has uses in React and Figma
+symbol => It has uses in React and Figma.
 */
 // object
 console.log(typeof age);
+
+
