5ae9547863461c9050b7c4a733226d2f3622f9fc
Deepak Yadav <iamdky2017@gmail.com>
2024-02-22 10:52:05 +0530

    Conversion of data types

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,13 +1,17 @@
-let score = "33abc"
+let score = "22true"
 
 // console.log(typeof score);
 // console.log(typeof(score));
-
-
-
 // ************* Operations ***************************
 
-let value =3
-let negvalue = -value
-console.log(negvalue);
+// let value =3
+// let negvalue = -value
+// console.log(negvalue);
+// console.log(typeof score);
 
+// "33" > Number can be converted easly.
+// "22abc" > NaN
+// true > 1; false > 0
+let isLoggedIn = 3
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
